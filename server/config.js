5d42a62445e147374953d21e1033fcbd82e4d1eb
Ankush Roy <imankushroy@gmail.com>
2023-09-06 19:22:52 +0530

    DB cnctn, routes-get all, create new, get specific

--- server/config.js
+++ server/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5000;
+export const mongoDBURL =
+  "mongodb+srv://root:root@bookstore.fugcz5u.mongodb.net/?retryWrites=true&w=majority";
--- server/index.js
+++ server/index.js
@@ -1,8 +1,79 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log("Server listening on port 5000...");
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Bookstore CRUD application");
 });
+
+//middleware
+app.use(express.json());
+
+//Route for a new book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message:
+          "Provide all the required fields : Title, Author, Publisher Year",
+      });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (err) {
+    console.log(err.message);
+    response.status(500).send({ message: err.message });
+  }
+});
+
+//Route for getting all books
+app.get("/books", async (req, res) => {
+  try {
+    const allBooks = await Book.find({});
+    return res.status(200).json({
+      count: allBooks.length,
+      data: allBooks,
+    });
+  } catch (err) {
+    console.log(err.message);
+    response.status(500).send({ message: err.message });
+  }
+});
+
+//Route for getting a specific Book by id
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+
+    return res.status(200).json(book);
+  } catch (err) {
+    console.log(err.message);
+    response.status(500).send({ message: err.message });
+  }
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Database Connected");
+
+    app.listen(PORT, () => {
+      console.log("Server listening on port 5000...");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
